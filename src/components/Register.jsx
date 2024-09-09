9442adc52ff3362ea9f193ddc8135d01bb30e07c
KARTHIKEYAN0807 <karthickestore.37sp@gmail.com>
2024-09-09 11:50:12 +0530

    frontend

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,7 +9,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', { email, password });
+      const res = await axios.post('https://password-reset-app-backend.onrender.com/api/auth/register', { email, password });
       setMessage(res.data.message);
     } catch (error) {
       setMessage(error.response.data.message);
